1813b3997d1046fc3621fa6776335552afb718c8
KristiyanGK <kristiangk@abv.bg>
2018-12-04 23:39:03 +0200

    Added default asp dot net core mvc template with scaffolded identity

---
+++ src/Vidos.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Vidos.Web.Areas.Identity.Data;
+using Vidos.Web.Models;
+
+[assembly: HostingStartup(typeof(Vidos.Web.Areas.Identity.IdentityHostingStartup))]
+namespace Vidos.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<VidosContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("VidosContextConnection")));
+
+                services.AddDefaultIdentity<VidosUser>()
+                    .AddEntityFrameworkStores<VidosContext>();
+            });
+        }
+    }
+}
